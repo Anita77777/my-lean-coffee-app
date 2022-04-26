aeb311cf1ff2b760991292e1bd0ba633638e0627
Anita Savic <savicanita500@gmail.com>
2022-04-26 14:45:54 +0200

    Add design

--- pages/src/components/Header.js
+++ pages/src/components/Header.js
@@ -1,13 +1,32 @@
+import { AppBar } from "@mui/material";
 import Link from "next/link";
+import { Button } from "@mui/material";
+import { Container } from "@mui/material";
+import { Toolbar } from "@mui/material";
 
 export default function Header() {
   return (
-    <header>
-      <Link href="/">Home</Link>
-      <br />
-      <Link href="/cards">Cards</Link>
-      <br />
-      <Link href="/create">Create</Link>
-    </header>
+    <AppBar color="primary">
+      <Container maxWidth="xl">
+        <Toolbar>
+          <Link href="/" passHref>
+            <Button component="a" sx={{ color: "white" }}>
+              Home
+            </Button>
+          </Link>
+
+          <Link href="/cards" passHref>
+            <Button component="a" sx={{ color: "white" }}>
+              Cards
+            </Button>
+          </Link>
+          <Link href="/create" passHref>
+            <Button component="a" sx={{ color: "white" }}>
+              Create
+            </Button>
+          </Link>
+        </Toolbar>
+      </Container>
+    </AppBar>
   );
 }
--- pages/src/components/Layout.js
+++ pages/src/components/Layout.js
@@ -1,10 +1,12 @@
 import Header from "./Header";
-
+import { Container } from "@mui/material";
 export default function Layout({ children }) {
   return (
     <>
       <Header />
-      {children}
+      <Container maxWidth="xl" sx={{ marginTop: "6rem" }}>
+        {children}
+      </Container>
     </>
   );
 }
